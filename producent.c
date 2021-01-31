a006b1415231a713588e321950f951d16c22fd4f
Maciej Nikiel <mcwojs@student.agh.edu.pl>
2021-01-31 20:29:54 +0100

    Add producent.h

--- producent.c
+++ producent.c
@@ -1,11 +1,4 @@
-#define _GNU_SOURCE
-
-#include "parse.h"
-
-#include <unistd.h>
-#include <stdint.h>
-
-int readInput( int argc, char *argv[], char *address, uint16_t *port, float *speed );
+#include "producent.h"
 
 int main( int argc, char *argv[] ) {
     char address[16] = "localhost";
@@ -15,6 +8,38 @@ int main( int argc, char *argv[] ) {
     readInput( argc, argv, address, &port, &speed );
 
     return 0;
+}
+
+int createServer(char* address, uint16_t port) {
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(serverSocket == -1) {
+        perror("Can't create server socket\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in addr;
+    memset(&addr,0,sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    int res = inet_aton(address, &addr.sin_addr);
+
+    if(res == -1) {
+        perror("Error int inet_aton\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(bind(serverSocket, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) == -1) {
+        perror("Can't bind server socket\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(serverSocket, 20) == -1) {
+        perror("Error in listen\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return serverSocket;
 }
 
 int readInput( int argc, char *argv[], char *address, uint16_t *port, float *speed ) {
---
+++ producent.h
@@ -0,0 +1,25 @@
+#ifndef HURT_BITOW_PRODUCENT_H
+#define HURT_BITOW_PRODUCENT_H
+
+#define _GNU_SOURCE
+
+#include "parse.h"
+#include "error_functions.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
+#include <poll.h>
+#include <time.h>
+#include <signal.h>
+#include <sys/timerfd.h>
+
+int readInput( int argc, char *argv[], char *address, uint16_t *port, float *speed );
+int createServer(char* address, uint16_t port);
+
+#endif //HURT_BITOW_PRODUCENT_H

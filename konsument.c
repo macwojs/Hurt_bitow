97eab5b20575829c13dcd955278d01a12d4e7341
Maciej Nikiel <mcwojs@student.agh.edu.pl>
2021-02-01 15:36:06 +0100

    Get data from server in konsument

--- konsument.c
+++ konsument.c
@@ -9,7 +9,77 @@ int main( int argc, char *argv[] ) {
 
     readInput( argc, argv, address, &port, &capacity, &download_speed, &degradation_speed );
 
+    int soc_fd = connectToServer( address, port );
+
+    getData( soc_fd, capacity, download_speed, degradation_speed );
+
     return 0;
+}
+
+
+void getData( int soc_fd, int capacity, float download_speed, float degradation_speed ) {
+    char send_buffer = 'r';
+    long long storage = capacity * STORAGE;
+    long long actual_storage = 0;
+    char buffer[13312] = {};
+    while ( 1 ) {
+        //Send request for data from server
+        if ( send( soc_fd, &send_buffer, sizeof( send_buffer ), NULL) == -1 ) {
+            perror( "Cant request for data" );
+            exit( EXIT_FAILURE );
+        }
+
+        //Get data from server
+        int recv_result = recv( soc_fd, buffer, sizeof( buffer ), NULL);
+        if ( recv_result == -1 && (errno == EAGAIN || errno == EWOULDBLOCK )) {
+            errno = 0;
+        } else if ( recv_result == -1 ) {
+            perror( "Cant read data from server" );
+            exit( EXIT_FAILURE );
+        }
+
+        //Processing and degradation
+        actual_storage += recv_result;
+
+        struct timespec ts = {};
+        size_t sleep_ns = 13312 / ( download_speed * 4435 ) * 1e9;
+        ts.tv_sec = sleep_ns / 1e9;
+        ts.tv_nsec = sleep_ns % ( size_t ) ( 1e9 );
+
+
+
+        nanosleep( &ts, NULL);
+
+
+        //TODO: Wait for process data
+        //TODO: Degradate data
+
+
+    }
+}
+
+int connectToServer( char *address, uint16_t port ) {
+    int client_socket = socket( AF_INET, SOCK_STREAM, 0 );
+    if ( client_socket == -1 ) {
+        perror( "Can't create client socket\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    struct sockaddr_in addr;
+    memset( &addr, 0, sizeof( struct sockaddr_in ));
+    addr.sin_port = htons( port );
+    addr.sin_family = AF_INET;
+    if ( inet_aton( address, &addr.sin_addr ) == -1 ) {
+        perror( "Error during parsing address\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    if ( connect( client_socket, &addr, sizeof( addr )) == -1 ) {
+        perror( "Error during connecting to server\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    return client_socket;
 }
 
 int readInput( int argc, char *argv[], char *address, uint16_t *port, int *capacity, float *download_speed,
@@ -29,10 +99,10 @@ int readInput( int argc, char *argv[], char *address, uint16_t *port, int *capac
                     break;
             }
         } else {
-            if (strlen(argv[ optind ])<=5){
+            if ( strlen( argv[ optind ] ) <= 5 ) {
                 *port = parseUInt16( argv[ optind ] );
                 optind++;
-            }else{
+            } else {
                 *port = parsePort( argv[ optind ] );
                 memcpy( address, parseAddress( argv[ optind ] ), strlen( parseAddress( argv[ optind ] )) + 1 );
                 optind++;
--- konsument.h
+++ konsument.h
@@ -7,10 +7,18 @@
 
 #include <unistd.h>
 #include <stdint.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/timerfd.h>
+
+#define STORAGE 30720 //30KiB
 
 
 int readInput( int argc, char *argv[], char *address, uint16_t *port, int *capacity, float *download_speed,
                float *degradation_speed );
 
+int connectToServer( char *address, uint16_t port );
+
+void getData( int soc_fd, int capacity, float download_speed, float degradation_speed );
 
 #endif //HURT_BITOW_KONSUMENT_H

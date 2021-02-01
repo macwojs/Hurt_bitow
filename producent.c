cab1adad422933204a24b4127f30d29d3f6bd8ba
Maciej Nikiel <mcwojs@student.agh.edu.pl>
2021-02-01 12:20:51 +0100

    Add pipe

--- producent.c
+++ producent.c
@@ -11,10 +11,60 @@ int main( int argc, char *argv[] ) {
 
     int epoll_fd = createEpoll( soc_fd );
 
+    int pipe_fd = forkProduce();
 
+    handleConnection(soc_fd, epoll_fd, pipe_fd);
 
+    close(soc_fd);
+    close(epoll_fd);
+    close(pipe_fd);
 
     return 0;
+}
+
+int forkProduce() {
+    int pipe_fd[2];
+    if(pipe(pipe_fd)==-1){
+        perror( "Can't create pipe" );
+        exit( EXIT_FAILURE );
+    }
+
+    int child = fork();
+    if(child == 0){
+        //TODO: obsluga magazynu
+    }
+
+    close(pipe_fd[1]);
+    return pipe_fd[0];
+}
+
+void handleConnection(int soc_fd, int epoll_fd, int pip_fd){
+    int ready;
+    struct epoll_event *evlist = calloc(EPOLL_WAIT_LIMIT, sizeof(struct epoll_event));
+
+    while(1){
+        ready = epoll_wait(epoll_fd, evlist, EPOLL_WAIT_LIMIT, -1);
+        if (ready == -1) {
+            if (errno == EINTR)
+                continue;
+            else{
+                perror( "Error during epoll wait" );
+                exit( EXIT_FAILURE );
+            }
+        }
+
+        for (int i = 0; i < ready; ++i){
+            if(evlist[i].data.u32 == 15){
+                //TODO: handle new connection
+            }else if(evlist[i].events & EPOLLIN){
+                //TODO: send new data
+            }else if(evlist[i].events & EPOLLHUP){
+                //TODO: connection droped
+            }
+        }
+    }
+
+    free(evlist);
 }
 
 int createEpoll( int soc_fd ) {
@@ -24,7 +74,7 @@ int createEpoll( int soc_fd ) {
         exit( EXIT_FAILURE );
     }
 
-    struct epoll_event ev;
+    struct epoll_event ev = {};
     ev.events = EPOLLIN;
     ev.data.fd = soc_fd;
     ev.data.u32 = 15;
@@ -32,6 +82,8 @@ int createEpoll( int soc_fd ) {
         perror( "Can't add descriptor to epoll" );
         exit( EXIT_FAILURE );
     }
+
+    return epoll_fd;
 }
 
 int createServer( char *address, uint16_t port ) {
@@ -56,6 +108,11 @@ int createServer( char *address, uint16_t port ) {
     if ( bind( serverSocket, ( struct sockaddr * ) &addr, sizeof( struct sockaddr_in )) == -1 ) {
         perror( "Can't bind server socket\n" );
         exit( EXIT_FAILURE );
+    }
+
+    if (listen(serverSocket, 20) == -1) {
+        perror("Error in listen\n");
+        exit(EXIT_FAILURE);
     }
 
     return serverSocket;
--- producent.h
+++ producent.h
@@ -18,10 +18,17 @@
 #include <signal.h>
 #include <sys/timerfd.h>
 
+#define CLIENT_LIMIT 1024
+#define EPOLL_WAIT_LIMIT (CLIENT_LIMIT + 1)
+
 int readInput( int argc, char *argv[], char *address, uint16_t *port, float *speed );
 
 int createServer( char *address, uint16_t port );
 
 int createEpoll( int soc_fd );
+
+int forkProduce();
+
+void handleConnection(int soc_fd, int epoll_fd, int pip_fd);
 
 #endif //HURT_BITOW_PRODUCENT_H

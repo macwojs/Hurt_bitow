ff70770c86d35f81668b3d956d90c5a8696123e7
Maciej Nikiel <mcwojs@student.agh.edu.pl>
2021-02-03 11:55:29 +0100

    Report ip and port after client finish

--- konsument.c
+++ konsument.c
@@ -1,6 +1,7 @@
 #include "konsument.h"
 
 typedef struct timespec timespec;
+struct sockaddr_in local_address;
 
 int main( int argc, char *argv[] ) {
     char address[16] = "localhost";
@@ -14,13 +15,13 @@ int main( int argc, char *argv[] ) {
     getData( capacity, download_speed, degradation_speed, address, port );
 
     struct timespec finish_time;
-    if (clock_gettime(CLOCK_REALTIME, &finish_time) == -1){
+    if ( clock_gettime( CLOCK_REALTIME, &finish_time ) == -1 ) {
         perror( "Error during get finish time" );
         exit( EXIT_FAILURE );
     }
 
-    fprintf(stderr, "Finish time %li sec, %li nsec", finish_time.tv_sec, finish_time.tv_nsec);
-    //TODO: print pid and address
+    fprintf( stderr, "Finish time %li sec, %li nsec", finish_time.tv_sec, finish_time.tv_nsec );
+    fprintf( stderr, "\t\t\t\t\t address: %s:%d\n", inet_ntoa( local_address.sin_addr ), ntohs( local_address.sin_port ));
 
     return 0;
 }
@@ -42,36 +43,36 @@ int getData( int capacity, float download_speed, float degradation_speed, char *
     while ( 1 ) {
         int soc_fd = connectToServer( address, port );
 
-        if (clock_gettime(CLOCK_MONOTONIC, &connect_time) == -1){
+        if ( clock_gettime( CLOCK_MONOTONIC, &connect_time ) == -1 ) {
             perror( "Error during get connect time" );
             exit( EXIT_FAILURE );
         }
 
         //Get data from server
-        for (int i =0; i<4; i++){
+        for ( int i = 0; i < 4; i++ ) {
 
-            int recv_result = read( soc_fd, buffer, sizeof( buffer ) );
+            int recv_result = read( soc_fd, buffer, sizeof( buffer ));
             if ( recv_result == -1 ) {
                 perror( "Cant read data from server" );
                 exit( EXIT_FAILURE );
             }
 
-            if(i==0){
-                if (clock_gettime(CLOCK_MONOTONIC, &first_package_time) == -1){
+            if ( i == 0 ) {
+                if ( clock_gettime( CLOCK_MONOTONIC, &first_package_time ) == -1 ) {
                     perror( "Error during get connect time" );
                     exit( EXIT_FAILURE );
                 }
             }
 
-            if(i==3){
-                if (clock_gettime(CLOCK_MONOTONIC, &last_package_time) == -1){
+            if ( i == 3 ) {
+                if ( clock_gettime( CLOCK_MONOTONIC, &last_package_time ) == -1 ) {
                     perror( "Error during get connect time" );
                     exit( EXIT_FAILURE );
                 }
 
                 report *report_data = ( report * ) calloc( 1, sizeof( report ));
-                report_data->a = ( timespec* ) calloc( 1, sizeof( timespec ));
-                report_data->b = ( timespec* ) calloc( 1, sizeof( timespec ));
+                report_data->a = ( timespec * ) calloc( 1, sizeof( timespec ));
+                report_data->b = ( timespec * ) calloc( 1, sizeof( timespec ));
 
                 report_data->a->tv_sec = first_package_time.tv_sec - connect_time.tv_sec;
                 report_data->a->tv_nsec = first_package_time.tv_nsec - connect_time.tv_nsec;
@@ -89,20 +90,22 @@ int getData( int capacity, float download_speed, float degradation_speed, char *
             nanosleep( &ts, NULL);
 
             //tutaj biodegraduje material
-            if(i==0){
-                if (clock_gettime(CLOCK_MONOTONIC, &last_check_time) == -1){
+            if ( i == 0 ) {
+                if ( clock_gettime( CLOCK_MONOTONIC, &last_check_time ) == -1 ) {
                     perror( "Error during get connect time" );
                     exit( EXIT_FAILURE );
                 }
-            }else{
-                if (clock_gettime(CLOCK_MONOTONIC, &now_time) == -1){
+            }
+            else {
+                if ( clock_gettime( CLOCK_MONOTONIC, &now_time ) == -1 ) {
                     perror( "Error during get connect time" );
                     exit( EXIT_FAILURE );
                 }
 
-                double deg = (double)now_time.tv_sec - (double)last_check_time.tv_sec + (double)(now_time.tv_nsec - last_check_time.tv_nsec) * 1e-9;
-                int degraded_data= (int)(deg * degradation_speed);
-                actual_storage -=degraded_data;
+                double deg = ( double ) now_time.tv_sec - ( double ) last_check_time.tv_sec +
+                             ( double ) ( now_time.tv_nsec - last_check_time.tv_nsec ) * 1e-9;
+                int degraded_data = ( int ) ( deg * degradation_speed );
+                actual_storage -= degraded_data;
 
                 last_check_time.tv_sec = now_time.tv_sec;
                 last_check_time.tv_nsec = now_time.tv_sec;
@@ -110,30 +113,36 @@ int getData( int capacity, float download_speed, float degradation_speed, char *
 
             //Processing and degradation
             actual_storage += recv_result;
-            }
+        }
 
-        if(storage - actual_storage < FULL_PACKAGE){
+        if ( storage - actual_storage < FULL_PACKAGE ) {
+            int addr_size = sizeof(local_address);
+            getsockname(soc_fd, &local_address, &addr_size);
+
+            close( soc_fd );
             return 0;
         }
 
-        close(soc_fd); //to powinien juz zrobic serwer, ale dla pewnosci
+        close( soc_fd );
     }
 }
 
-void on_exit_report( int status, void *dn ){
+void on_exit_report( int status, void *dn ) {
     report *data = ( report * ) dn;
-    if( data->a->tv_nsec < 0){
+    if ( data->a->tv_nsec < 0 ) {
         data->a->tv_sec -= 1;
         data->a->tv_nsec = 1e9 - data->a->tv_nsec;
     }
 
-    if( data->b->tv_nsec < 0){
+    if ( data->b->tv_nsec < 0 ) {
         data->b->tv_sec -= 1;
         data->b->tv_nsec = 1e9 - data->a->tv_nsec;
     }
 
-    fprintf(stderr, "\n\nDelay between connect and first package: %li sec and %li nsec\n", data->a->tv_sec, data->a->tv_nsec);
-    fprintf(stderr, "Delay between first package and last package: %li sec and %li nsec\n", data->b->tv_sec, data->b->tv_nsec);
+    fprintf( stderr, "\n\nDelay between connect and first package: %li sec and %li nsec\n", data->a->tv_sec,
+             data->a->tv_nsec );
+    fprintf( stderr, "Delay between first package and last package: %li sec and %li nsec\n", data->b->tv_sec,
+             data->b->tv_nsec );
 }
 
 int connectToServer( char *address, uint16_t port ) {
@@ -176,11 +185,13 @@ int readInput( int argc, char *argv[], char *address, uint16_t *port, int *capac
                     *degradation_speed = parseFloat( optarg );
                     break;
             }
-        } else {
+        }
+        else {
             if ( strlen( argv[ optind ] ) <= 5 ) {
                 *port = parseUInt16( argv[ optind ] );
                 optind++;
-            } else {
+            }
+            else {
                 *port = parsePort( argv[ optind ] );
                 memcpy( address, parseAddress( argv[ optind ] ), strlen( parseAddress( argv[ optind ] )) + 1 );
                 optind++;
